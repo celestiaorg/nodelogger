44d51288d3971feed3eafb57b45f5de3fec79ed4
Mojtaba <mojtaba.esk@gmail.com>
2023-01-04 19:24:22 +0330

    build info added to the default endpoint

--- api/v1/index.go
+++ api/v1/index.go
@@ -4,19 +4,52 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"runtime/debug"
 )
 
 // IndexPage implements GET /
 func (a *RESTApiV1) IndexPage(resp http.ResponseWriter, req *http.Request) {
 
-	homeHTML := "Ciao, this is `NodeLogger` :) <p>"
-	allAPIs := a.GetAllAPIs()
-	for _, a := range allAPIs {
-		homeHTML += fmt.Sprintf(`<a href="%s">%s</a><br />`, a, a)
+	modName := "unknown"
+	buildInfo := ""
+	if bi, ok := debug.ReadBuildInfo(); ok {
+		modName = bi.Path
+
+		buildInfo += "<br /><h3>Build Info:</h3><table>"
+		for _, s := range bi.Settings {
+			buildInfo += fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>", s.Key, s.Value)
+		}
+		buildInfo += "</table>"
 	}
 
+	html := `<!DOCTYPE html><html><head><style>
+	table {border-collapse: collapse; width: 100%;}
+	td, th {border: 1px solid #222;text-align: left; padding: 8px;}
+	tr:nth-child(even) {background-color: #222;}
+	a {
+		text-decoration:none;border-bottom: 2px solid #10747f;
+		color: #f1ff8f;transition: background 0.1s cubic-bezier(.33,.66,.66,1);
+	}
+	a:hover {background: #10747f;}
+	body {
+		color: #FFF; font-family: sans-serif;
+		justify-content: center;align-items: center;
+		line-height:1.8;margin:0;padding:0 40px;
+		background-image: linear-gradient(135deg, rgba(0, 0, 0, 0.85) 0%,rgba(0, 0, 0,1) 100%);
+	  }
+	</style></head><body>`
+
+	html += fmt.Sprintf("Ciao, this is `%v` \n\n<p>", modName)
+	allAPIs := a.GetAllAPIs()
+	html += "<h3>List of endpoints:</h3>"
+	for _, a := range allAPIs {
+		html += fmt.Sprintf(`<a href="%s">%s</a><br />`, a, a)
+	}
+
+	html += buildInfo
+
 	resp.Header().Set("Content-Type", "text/html; charset=utf-8")
-	resp.Write([]byte(homeHTML))
+	resp.Write([]byte(html))
 }
 
 func (a *RESTApiV1) UI(resp http.ResponseWriter, req *http.Request) {
